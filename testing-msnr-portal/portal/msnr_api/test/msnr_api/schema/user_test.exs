fbf8af8dd1201e87ef1e367533143a2111260142
Ana Petrovic <pana.petrovic@gmail.com>
2023-04-27 12:16:02 +0200

    izmene teksta

--- testing-msnr-portal/portal/msnr_api/test/msnr_api/schema/user_test.exs
+++ testing-msnr-portal/portal/msnr_api/test/msnr_api/schema/user_test.exs
@@ -1,6 +1,14 @@
 defmodule MsnrApi.Schema.UserTest do
   use ExUnit.Case
+  alias Ecto.Changeset
   alias MsnrApi.Accounts.User
+
+  @required_fields [
+    :email,
+    :first_name,
+    :last_name,
+    :role
+  ]
 
   @expected_fields_with_types [
     {:id, :id},
@@ -45,6 +53,73 @@ defmodule MsnrApi.Schema.UserTest do
 
         assert MapSet.new(actual_fields_with_types) == MapSet.new(@expected_fields_with_types)
     end
+  end
+
+  describe "changeset/2" do
+    test "success: returns a valid changeset when given valid arguments" do
+      valid_params = %{
+        "email" => "ana@gmail.com",
+        "first_name" => "Name",
+        "last_name" => "LastName",
+        "role" => "student"
+      }
+      changeset = User.changeset(%User{}, valid_params)
+
+      assert %Changeset{valid?: true, changes: changes} = changeset
+
+      for {field, } <- @expected_fields_with_types do
+        actual = Map.get(changes, field)
+        expected = valid_params[Atom.to_string(field)]
+        assert actual == expected,
+          "Values did not match for: #{field}\nexpected: #{inspect(expected)}\nactual: #{inspect(actual)}"
+      end
+    end
+
+    test "error: returns an error changeset when given uncastable values" do
+      invalid_params = %{
+        "email" => 1,
+        "first_name" => 2,
+        "last_name" => 3,
+        "role" => 4
+      }
+
+      assert %Changeset{valid?: false, errors: errors} = User.changeset(%User{}, invalid_params)
+
+      for {field, _} <- @required_fields do
+        assert errors[field], "the field: #{field} is missing from errors."
+
+        {_, meta} = errors[field]
+        assert meta[:validation] == :cast,
+          "The validation type. #{meta[:validaiton]}, is incorrect."
+      end
+    end
+
+    test "error: returns an error changeset when given invalid role" do
+      invalid_params = %{
+        "email" => "email",
+        "first_name" => "ana",
+        "last_name" => "petrovic",
+        "role" => "not student and not professor"
+      }
+
+      assert %Changeset{valid?: false, errors: errors} = User.changeset(%User{}, invalid_params)
+    end
+
+    test "error: returns an error changeset when required fields are missing" do
+      params = %{}
+
+      assert %Changeset{valid?: false, errors: errors} = User.changeset(%User{}, params)
+
+      for {field, _} <- @expected_fields_with_types, field not in @optional do
+        assert errors[field], "the field: #{field} is missing from errors."
+
+        {_, meta} = errors[field]
+
+        assert meta[:validation] == :required,
+          "The validation type. #{meta[:validaiton]}, is incorrect."
+      end
+    end
+
   end
 
 end
